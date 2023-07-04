fd5193f011be82151e16f62e56c24d0dafab9ff7
Lorenzo Carli <lorenzo.carli@ennova-research.com>
2023-07-04 15:51:52 +0200

    add electron

---
+++ main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
+
+function onReady () {
+	win = new BrowserWindow({width: 900, height: 670})
+	win.loadURL(url.format({
+		pathname: path.join(
+			__dirname,
+			'dist/typelocke_dex/index.html'),
+		protocol: 'file:',
+		slashes: true
+	}))
+
+}
+
+app.on('ready', onReady);
