c56dc374715fdabbf32437d0a22bd539bddd2a97
lorenzo.carli <lorenzo.carli@ennova-research.com>
2023-12-24 17:36:36 +0100

    program icon

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ const url = require('url');
 const path = require('path');
 
 function onReady () {
-  //titleBarStyle: hidden, width, height, icon: "", frame: false,
   //width 809 works good too
   //mobile example 412 x 915
-	win = new BrowserWindow({width: 1200, height: 800, webPreferences: {nodeIntegration: true, contextIsolation: false}, autoHideMenuBar: true})
+	win = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		webPreferences: {nodeIntegration: true, contextIsolation: false},
+		autoHideMenuBar: true,
+		icon: "./src/assets/images/icons/logoTD.png"
+	})
 	win.loadURL(url.format({
 		pathname: path.join(
 			__dirname,
